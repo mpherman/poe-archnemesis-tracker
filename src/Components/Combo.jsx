2cfe4d67b258c56e42d388650ec1eac21ad1e83f
mpherman <max.pherman@gmail.com>
2022-02-17 13:52:05 -0500

    Add combo monsters with reward icons

--- src/Components/Combo.jsx
+++ src/Components/Combo.jsx
@@ -1,14 +1,63 @@
-import { Grid } from "@mui/material";
+import { Grid, IconButton, Paper } from "@mui/material";
+import Monsters from '../Util/Monsters';
+import {
+  MinusSquare
+} from "react-feather";
 
-function Combo({combo}) {
-
+function Monster({name, img, rewards, removeFromCombo}) {
+    function handleRemoveFromCombo(event) {
+        removeFromCombo(name);
+    }
+    const rewardImages = rewards.map((reward) => {
+        const rewardImg = "rewards/" + reward + ".png";
+        return (
+            <Grid item xs={3}>
+                <img src={rewardImg} alt={reward} className="reward-icon"/>
+            </Grid>
+        )
+    })
     return (
-        <div>
+        <Grid className="monster-grid-item" container spacing={0}>
+            <Grid item xs={2}>
+               <Grid item xs={12}>
+                    <img src={img} alt={name} />
+                </Grid>
+                <Grid className="monster-grid-button" item xs={12}>
+                    <IconButton onClick={handleRemoveFromCombo}>
+                        <MinusSquare className="monster-grid-combo-remove-button" />
+                    </IconButton>
+                </Grid>
+            </Grid>
+            <Grid container xs={10}>
+                <Grid className="monster-grid-title" item xs={12}>
+                    {name}
+                </Grid>
+                {rewardImages}
+            </Grid>
+        </Grid>
+    )
+}
+
+
+function Combo({combo, removeFromCombo}) {
+    const ComboPieces = combo.map((monster) => {
+        const imageSrc = Monsters[monster].img;
+        const rewards = Monsters[monster].rewards;
+        return (
+            <Grid item p={0} xs={3} key={monster}>
+                <Paper elevation={3}>
+                    <Monster name={monster} img={imageSrc} rewards={rewards} removeFromCombo={removeFromCombo}/>
+                </Paper>
+            </Grid>
+        );
+    })
+    return (
+        <div className="monster-combo-box">
             <h2>
                 Combo
             </h2>
             <Grid container spacing={6} p={0}>
-                {"test"}
+                {ComboPieces}
             </Grid>
         </div>
     )
--- src/Util/Monsters.js
+++ src/Util/Monsters.js
@@ -1,192 +1,433 @@
 const Monsters = {
     "Arcane Buffer": {
-        "img": "monsters/arcane-buffer.png"
+        "img": "monsters/arcane-buffer.png",
+        "rewards": [
+            "essences"
+        ],
     },
     "Berserker": {
-        "img": "monsters/berserker.png"
+        "img": "monsters/berserker.png",
+        "rewards": [
+            "uniques"
+        ],
     },
     "Bloodletter": {
-        "img": "monsters/bloodletter.png"
+        "img": "monsters/bloodletter.png",
+        "rewards": [
+            "weapons",
+            "jewellry"
+        ],
     },
     "Bombardier": {
-        "img": "monsters/bombardier.png"
+        "img": "monsters/bombardier.png",
+        "rewards": [
+            "weapons",
+            "armour"
+        ],
     },
     "Bonebreaker": {
-        "img": "monsters/bonebreaker.png"
+        "img": "monsters/bonebreaker.png",
+        "rewards": [
+            "random",
+            "weapon"
+        ],
     },
     "Chaosweaver": {
-        "img": "monsters/chaosweaver.png"
+        "img": "monsters/chaosweaver.png",
+        "rewards": [
+            "gems"
+        ],
     },
     "Consecrator": {
-        "img": "monsters/consecrator.png"
+        "img": "monsters/consecrator.png",
+        "rewards": [
+            "fragments"
+        ],
     },
     "Deadeye": {
-        "img": "monsters/deadeye.png"
+        "img": "monsters/deadeye.png",
+        "rewards": [
+            "armour",
+            "jewellry"
+        ],
     },
     "Dynamo": {
-        "img": "monsters/dynamo.png"
+        "img": "monsters/dynamo.png",
+        "rewards": [
+            "random",
+            "jewellry"
+        ],
     },
     "Echoist": {
-        "img": "monsters/echoist.png"
+        "img": "monsters/echoist.png",
+        "rewards": [
+            "random",
+            "currency"
+        ],
     },
     "Flameweaver": {
-        "img": "monsters/flameweaver.png"
+        "img": "monsters/flameweaver.png",
+        "rewards": [
+            "weapons"
+        ],
     },
     "Frenzied": {
-        "img": "monsters/frenzied.png"
+        "img": "monsters/frenzied.png",
+        "rewards": [
+            "random",
+            "uniques"
+        ],
     },
     "Frostweaver": {
-        "img": "monsters/frostweaver.png"
+        "img": "monsters/frostweaver.png",
+        "rewards": [
+            "armour"
+        ],
     },
     "Gargantuan": {
-        "img": "monsters/gargantuan.png"
+        "img": "monsters/gargantuan.png",
+        "rewards": [
+            "currency"
+        ],
     },
     "Hasted": {
-        "img": "monsters/hasted.png"
+        "img": "monsters/hasted.png",
+        "rewards": [
+            "random"
+        ],
     },
     "Incendiary": {
-        "img": "monsters/incendiary.png"
+        "img": "monsters/incendiary.png",
+        "rewards": [
+            "random",
+            "weapons"
+        ],
     },
     "Juggernaut": {
-        "img": "monsters/juggernaut.png"
+        "img": "monsters/juggernaut.png",
+        "rewards": [
+            "harbinger"
+        ],
     },
     "Malediction": {
-        "img": "monsters/malediction.png"
+        "img": "monsters/malediction.png",
+        "rewards": [
+            "divination-cards"
+        ],
     },
     "Opulent": {
-        "img": "monsters/opulent.png"
+        "img": "monsters/opulent.png",
+        "rewards": [
+            
+        ],
     },
     "Overcharged": {
-        "img": "monsters/overcharged.png"
+        "img": "monsters/overcharged.png",
+        "rewards": [
+            "jewellry",
+            "jewellry"
+        ],
     },
     "Permafrost": {
-        "img": "monsters/permafrost.png"
+        "img": "monsters/permafrost.png",
+        "rewards": [
+            "random",
+            "armour"
+        ],
     },
     "Sentinel": {
-        "img": "monsters/sentinel.png"
+        "img": "monsters/sentinel.png",
+        "rewards": [
+            "armour",
+            "armour"
+        ],
     },
     "Soul Conduit": {
-        "img": "monsters/soul-conduit.png"
+        "img": "monsters/soul-conduit.png",
+        "rewards": [
+            "maps"
+        ],
     },
     "Steel-Infused": {
-        "img": "monsters/steel-infused.png"
+        "img": "monsters/steel-infused.png",
+        "rewards": [
+            "weapons"
+        ],
     },
     "Stormweaver": {
-        "img": "monsters/stormweaver.png"
+        "img": "monsters/stormweaver.png",
+        "rewards": [
+            "jewellry"
+        ],
     },
     "Toxic": {
-        "img": "monsters/toxic.png"
+        "img": "monsters/toxic.png",
+        "rewards": [
+            "random",
+            "gems"
+        ],
     },
     "Vampiric": {
-        "img": "monsters/vampiric.png"
+        "img": "monsters/vampiric.png",
+        "rewards": [
+            "fossils"
+        ],
     },
     "Rejuvenating": {
-        "img": "monsters/rejuvenating.png"
+        "img": "monsters/rejuvenating.png",
+        "rewards": [
+            "currency"
+        ],
     },
     "Corrupter": {
-        "img": "monsters/corrupter.png"
+        "img": "monsters/corrupter.png",
+        "rewards": [
+            "abyss",
+            "abyss"
+        ],
     },
     "Assassin": {
-        "img": "monsters/assassin.png"
+        "img": "monsters/assassin.png",
+        "rewards": [
+            "currency",
+            "currency"
+        ],
     },
     "Hexer": {
-        "img": "monsters/hexer.png"
+        "img": "monsters/hexer.png",
+        "rewards": [
+            "essences",
+            "essences"
+        ],
     },
     "Entangler": {
-        "img": "monsters/entangler.png"
+        "img": "monsters/entangler.png",
+        "rewards": [
+            "fossils",
+            "fossils"
+        ],
     },
     "Necromancer": {
-        "img": "monsters/necromancer.png"
+        "img": "monsters/necromancer.png",
+        "rewards": [
+            "generic"
+        ],
     },
     "Drought Bringer": {
-        "img": "monsters/drought-bringer.png"
+        "img": "monsters/drought-bringer.png",
+        "rewards": [
+            "labyrinth",
+            "labyrinth"
+        ],
     },
     "Evocationist": {
-        "img": "monsters/evocationist.png"
+        "img": "monsters/evocationist.png",
+        "rewards": [
+            "random",
+            "weapons",
+            "armour",
+            "jewellry"
+        ],
     },
     "Treant Horde": {
-        "img": "monsters/treant-horde.png"
+        "img": "monsters/treant-horde.png",
+        "rewards": [
+            "random"
+        ],
     },
     "Invulnerable": {
-        "img": "monsters/invulnerable.png"
+        "img": "monsters/invulnerable.png",
+        "rewards": [
+            "delirium",
+            "metamorph"
+        ],
     },
     "Frost Strider": {
-        "img": "monsters/frost-strider.png"
+        "img": "monsters/frost-strider.png",
+        "rewards": [
+            "armour",
+            "armour",
+            "armour"
+        ],
     },
     "Storm Strider": {
-        "img": "monsters/storm-strider.png"
+        "img": "monsters/storm-strider.png",
+        "rewards": [
+            "jewellry",
+            "jewellry",
+            "jewellry"
+        ],
     },
     "Flame Strider": {
-        "img": "monsters/flame-strider.png"
+        "img": "monsters/flame-strider.png",
+        "rewards": [
+            "weapons",
+            "weapons",
+            "weapons"
+        ],
     },
     "Executioner": {
-        "img": "monsters/executioner.png"
+        "img": "monsters/executioner.png",
+        "rewards": [
+            "legion",
+            "breach"
+        ],
     },
     "Magma Barrier": {
-        "img": "monsters/magma-barrier.png"
+        "img": "monsters/magma-barrier.png",
+        "rewards": [
+            "weapons",
+            "weapons"
+        ],
     },
     "Ice Prison": {
-        "img": "monsters/ice-prison.png"
+        "img": "monsters/ice-prison.png",
+        "rewards": [
+            "armour",
+            "armour"
+        ],
     },
     "Heralding Minions": {
-        "img": "monsters/heralding-minions.png"
+        "img": "monsters/heralding-minions.png",
+        "rewards": [
+            "fragments",
+            "fragments"
+        ],
     },
     "Mana Siphoner": {
-        "img": "monsters/mana-siphoner.png"
+        "img": "monsters/mana-siphoner.png",
+        "rewards": [
+            "jewellry",
+            "jewellry"
+        ],
     },
     "Mirror Image": {
-        "img": "monsters/mirror-image.png"
+        "img": "monsters/mirror-image.png",
+        "rewards": [
+            "scarabs"
+        ],
+        "bonus": [
+            "Rewards are rolled 2 additional times, choosing the rarest result"
+        ]
     },
     "Empowering Minions": {
-        "img": "monsters/empowering-minions.png"
+        "img": "monsters/empowering-minions.png",
+        "rewards": [
+            "blight",
+            "ritual"
+        ],
     },
     "Effigy": {
-        "img": "monsters/effigy.png"
+        "img": "monsters/effigy.png",
+        "rewards": [
+            "divination-cards",
+            "divination-cards"
+        ],
     },
     "Crystal-Skinned": {
-        "img": "monsters/crystal-skinned.png"
+        "img": "monsters/crystal-skinned.png",
+        "rewards": [
+            "harbinger",
+            "harbinger"
+        ],
     },
     "Temporal Bubble": {
-        "img": "monsters/temporal-bubble.png"
+        "img": "monsters/temporal-bubble.png",
+        "rewards": [
+            "height",
+            "expedition"
+        ],
     },
     "Empowered Elements": {
-        "img": "monsters/empowered-elements.png"
+        "img": "monsters/empowered-elements.png",
+        "rewards": [
+            "uniques",
+            "uniques"
+        ],
     },
     "Trickster": {
-        "img": "monsters/trickster.png"
+        "img": "monsters/trickster.png",
+        "rewards": [
+            "currency",
+            "uniques",
+            "divination-cards"
+        ],
     },
     "Soul Eater": {
-        "img": "monsters/soul-eater.png"
+        "img": "monsters/soul-eater.png",
+        "rewards": [
+            "maps",
+            "maps"
+        ],
     },
     "Tukohama-touched": {
-        "img": "monsters/tukohama-touched.png"
+        "img": "monsters/tukohama-touched.png",
+        "rewards": [
+            "weapons",
+            "weapons",
+            "fragments"
+        ],
     },
     "Corpse Detonator": {
-        "img": "monsters/corpse-detonator.png"
+        "img": "monsters/corpse-detonator.png",
+        "rewards": [
+            "divination-cards",
+            "divination-cards"
+        ],
     },
     "Abberath-touched": {
-        "img": "monsters/abberath-touched.png"
+        "img": "monsters/abberath-touched.png",
+        "rewards": [
+            "jewellry",
+            "jewellry",
+            "maps"
+        ],
     },
     "Lunaris-touched": {
-        "img": "monsters/lunaris-touched.png"
+        "img": "monsters/lunaris-touched.png",
+        "rewards": [
+            "unqiues"
+        ],
     },
     "Solaris-touched": {
-        "img": "monsters/solaris-touched.png"
+        "img": "monsters/solaris-touched.png",
+        "rewards": [
+            "scarabs"
+        ],
     },
     "Arakaali-touched": {
-        "img": "monsters/arakaali-touched.png"
+        "img": "monsters/arakaali-touched.png",
+        "rewards": [
+            "divination-cards"
+        ],
     },
     "Innocence-touched": {
-        "img": "monsters/innocence-touched.png"
+        "img": "monsters/innocence-touched.png",
+        "rewards": [
+            "currency",
+            "currency",
+            "currency"
+        ],
     },
     "Shakari-touched": {
-        "img": "monsters/shakari-touched.png"
+        "img": "monsters/shakari-touched.png",
+        "rewards": [
+            "uniques"
+        ],
     },
     "Kitava-touched": {
-        "img": "monsters/kitava-touched.png"
+        "img": "monsters/kitava-touched.png",
+        "rewards": [
+            "random"
+        ],
     },
     "Brine King-touched": {
-        "img": "monsters/brine-king-touched.png"
+        "img": "monsters/brine-king-touched.png",
+        "rewards": [
+            "armour",
+            "armour",
+            "armour"
+        ],
     }
 };
 
