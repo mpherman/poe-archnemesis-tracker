eb7da5c4c064b139a90e885d0fd5632ae632ba40
mpherman <max.pherman@gmail.com>
2022-02-16 23:40:57 -0500

    Add StylesProvider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StylesProvider } from '@mui/styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <StylesProvider injectFirst>
+        <App />
+      </StylesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
